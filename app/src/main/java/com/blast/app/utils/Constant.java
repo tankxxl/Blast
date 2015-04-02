dbb1b27de40e917b8a46c71556e91a887bdd8f4a
rengangzai <tank2xxl@gmail.com>
2015-04-02 18:40:11 +0800

    modify fieldname.

--- app/src/main/java/com/blast/app/utils/Constant.java
+++ app/src/main/java/com/blast/app/utils/Constant.java
@@ -1,9 +1,3 @@
-/**
- *  软件版权：UMPAY
- *  系统名称：12580SDK
- *  文件名称：Constant.java
- *  版本变更记录（可选）：修改日期2013-11-11  下午3:04:34，修改人rgz，工单号（手填），修改描述（手填）
- */
 package com.blast.app.utils;
 
 
@@ -23,8 +17,6 @@ public class Constant {
 
 	// String
 	public static final class string {
-//		public static final String hfb_app_name = "提示";
-		
 //		public static final String title_dlg = "提示";
 //		public static final String ok_lbl = "确定";
 //		public static final String cancel_lbl = "取消";
@@ -35,23 +27,11 @@ public class Constant {
 	public static final class color {
 		public static final String gray_color = "#676767";
 		public static final String black_color = "#1c1c1c";
-		public static final String hfb_content_bg = "#FFFFFF";
-	    public static final String hfb_umpay_info = "#777777";
+		public static final String content_bg = "#FFFFFF";
+	    public static final String info = "#777777";
 	    
-	    public static final String hfb_red = "#f94110";
-	    public static final String hfb_green = "#7ea512";
+	    public static final String red = "#f94110";
+	    public static final String green = "#7ea512";
 	}
-
-	// <color name="hfb_content_bg">#FFFFFF</color>
-	// <color name="hfb_sms_info_bg">#3b6482</color>
-	// <color name="hfb_umpay_info">#777777</color>
-	/*
-	 * // prepare int strokeWidth = 5; // 3dp int roundRadius = 15; // 8dp int
-	 * strokeColor = Color.parseColor("#2E3135"); int fillColor =
-	 * Color.parseColor("#DFDFE0");
-	 * 
-	 * GradientDrawable gd = new GradientDrawable(); gd.setColor(fillColor);
-	 * gd.setCornerRadius(roundRadius); gd.setStroke(strokeWidth, strokeColor);
-	 */
 
 }
